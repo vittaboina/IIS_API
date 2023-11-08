c77bb81590afbcc54a6e082d9ad1e355af9093a2
vittaboina <vittaboina@gmail.com>
2023-11-08 09:41:40 -0500

    Solution added

--- IIS_API/Controllers/FecilityController.cs
+++ IIS_API/Controllers/FecilityController.cs
@@ -12,7 +12,7 @@ namespace IIS_API.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value3" };
         }
 
         // GET api/<FecilityController>/5
